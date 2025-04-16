072c39d5b7dd633cee082e2d7c1a4e17abbfa580
90mahmoud <83636588+90mahmoud@users.noreply.github.com>
2025-04-16 22:46:29 +0200

    add all files

---
+++ Scripts/General/AjaxCall.js
@@ -0,0 +1,28 @@
+﻿var Ajax = {
+    AjaxCall: function (ActionPath, Data, SuccessCallBack, ErrorCallBack, OtherParams) {
+        event.preventDefault();
+        event.stopImmediatePropagation();
+        var NewData = (Data.constructor == Function) ? Data() : Data;
+        NewData.append("Token", localStorage["qmmtkn"]);
+        $.ajax({
+            type: "POST",
+            url: (ActionPath.constructor == Function) ? ActionPath() : ActionPath,
+            data: NewData,
+            dataType: "json",
+            contentType: false,
+            processData: false,
+            async: false,
+            success: function (result) {
+                // Result.
+                if (SuccessCallBack)
+                    SuccessCallBack(result);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                if (ErrorCallBack)
+                    ErrorCallBack(jqXHR, textStatus, errorThrown);
+                //do your own thing
+                alert("fail");
+            }
+        });
+    },
+}
---
+++ Scripts/Modules/Login.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+    $("#frm_login").submit(function (event) {
+        var dataString;
+        var action = $("#frm_login").attr("action");
+        dataString = new FormData($("#frm_login").get(0));
+        Ajax.AjaxCall(action, dataString, onAjaxRequestSuccess);
+    });
+});
+var onAjaxRequestSuccess = function (result) {
+    if (result.ResultFlag == "1")
+        localStorage["qmmtkn"] = result.ResultTKN;
+    else {
+        var dv_error_message = document.getElementById("dv_error_message");
+        if (dv_error_message) {
+            var msg = "";
+            if (result.Errors) {
+                for (var i = 0; i < result.Errors.length; i++) {
+                    msg += "<p>" + result.Errors[i].Value + "</p>";
+                }
+            }
+            else
+                msg = result.ResultMSG;
+            dv_error_message.innerHTML = msg;
+            dv_error_message.style.display = "";
+        }
+    }
+}
